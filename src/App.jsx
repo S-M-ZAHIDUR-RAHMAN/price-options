748fbceeae0df573b45e71a3894e570c78929d8c
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-09-19 00:14:10 +0600

    added Link and NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import DaisyNav from './components/DaisyNav/DaisyNav.jsx'
+// import DaisyNav from './components/DaisyNav/DaisyNav.jsx'
+import NavBar from './components/NavBar/NavBar'
 function App() {
 
 
   return (
     <>
-    <DaisyNav></DaisyNav>
+    {/* <DaisyNav></DaisyNav> */}
+    <NavBar></NavBar>
       <h1 className='flex justify-center text-7xl font-bold'>Project</h1>
       
     </>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Link from '../Link/Link';
 
 const NavBar = () => {
-    const routes  = [
-            { name: 'Home', id: 1, path: '/' },
-            { name: 'About', id: 2, path: '/about' },
-            { name: 'Contact', id: 3, path: '/contact' },
-            { name: 'Services', id: 4, path: '/services' },
-            { name: 'Portfolio', id: 5, path: '/portfolio' }
-          ]
-          
-    
+    const routes = [
+        { name: 'Home', id: 1, path: '/' },
+        { name: 'About', id: 2, path: '/about' },
+        { name: 'Contact', id: 3, path: '/contact' },
+        { name: 'Services', id: 4, path: '/services' },
+        { name: 'Portfolio', id: 5, path: '/portfolio' }
+    ]
+
+
     return (
         <nav>
-            {
-                routes.map(route =>(
-                    
-                ))
-            }
-
+            <ul className='md:flex'> 
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
         </nav>
     );
 };
